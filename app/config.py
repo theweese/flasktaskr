4ccd63e3d85f4695594e180223ded6c8bb87392d
Brett Weese <brett.weese@gmail.com>
2014-10-09 12:08:37 -0500

    Commit before running db_create after changing everything to SQLAlchemy

--- app/config.py
+++ app/config.py
@@ -12,3 +12,6 @@ WTF_CSRF_ENABLED = 'TRUE'
 SECRET_KEY = 'my_precious'
 
 DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+# the database uri (being used for SQLAlchemy)
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
--- app/db_create.py
+++ app/db_create.py
@@ -1,22 +1,15 @@
 # db_create.py
 
-import sqlite3
-from config import DATABASE_PATH
+from views import db
+from models import Task
+from datetime import date
 
-with sqlite3.connect(DATABASE_PATH) as connection:
-	c = connection.cursor()
+# creates the database and the table
+db.create_all()
 
-	c.execute("""CREATE TABLE tasks(task_id INTEGER PRIMARY KEY
-		AUTOINCREMENT,
-		name TEXT NOT NULL, due_date TEXT NOT NULL, priority
-			INTEGER NOT NULL,
-		status INTEGER NOT NULL)""")
+# test data to insert
+db.session.add(Task("Finish this tutorial", date(2014, 3, 13), 10, 1))
+db.session.add(Task("Finish Real Python", date(2014, 3, 13), 10, 1))
 
-	c.execute(
-		'INSERT INTO tasks (name, due_date, priority, status)'
-		'VALUES("Finish this tutorial", "02/03/2014", 10, 1)'
-		)
-	c.execute(
-		'INSERT INTO tasks (name, due_date, priority, status)'
-		'VALUES("Finish Real Python Course 2", "02/03/2014", 10, 1)'
-		)
+# as usual commit your data
+db.session.commit()
